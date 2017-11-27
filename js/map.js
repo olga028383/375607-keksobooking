941825f40e179ef408bb055c671d541d2a6f7944
Olga <olga028383@mail.ru>
2017-11-27 20:58:33 +0300

    Has made code adjustments

--- js/map.js
+++ js/map.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var map = document.querySelector('.map');
-var similarListElementLabel = map.querySelector('.map__pins');
-var similarListElementAds = map.querySelector('.map__filters-container');
-var similarMapTemplate = document.querySelector('#similar-map-template').content;
-var similarMapTemplateLabel = similarMapTemplate.querySelector('.map__pin');
-var similarMapTemplateAds = similarMapTemplate.querySelector('.map__card');
+var mapPinContainer = map.querySelector('.map__pins');
+var mapFiltersContainer = map.querySelector('.map__filters-container');
+var mapTemplateContainer = document.querySelector('template').content;
+var mapPinTemplate = mapTemplateContainer.querySelector('.map__pin');
+var mapCardTemplate = mapTemplateContainer.querySelector('.map__card');
 var ads;
-var lengthMap = 8;
+var adsQuantity = 8;
 
 var adFeatures = {
   titles: ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домиик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'],
@@ -17,16 +17,8 @@ var adFeatures = {
   features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']
 };
 
-var getRandomArrayIndex = function (array) {
-  return Math.floor(Math.random() * array.length);
-};
-
-var getRandomInt = function (min, max) {
+var getRandomInteger = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-var showElement = function (element) {
-  element.classList.remove('map--faded');
 };
 
 var generateArrayMapPoints = function (objPoint, arrayLength) {
@@ -47,18 +39,18 @@ var generateArrayMapPoints = function (objPoint, arrayLength) {
   var photos;
   var i;
   for (i = 0; i < arrayLength; i++) {
-    locationX = getRandomInt(300, 900);
-    locationY = getRandomInt(100, 500);
+    locationX = getRandomInteger(300, 900);
+    locationY = getRandomInteger(100, 500);
     avatar = 'img/avatars/user0' + Number(i + 1) + '.png';
     title = objPoint.titles[i];
     address = locationX + ', ' + locationY;
-    price = getRandomInt(1000, 1000000);
-    type = objPoint.types[getRandomArrayIndex(objPoint.types)];
-    rooms = getRandomInt(1, 5);
-    guests = getRandomInt(10, 100);
-    checkin = objPoint.checkins[getRandomArrayIndex(objPoint.checkins)];
-    checkout = objPoint.checkouts[getRandomArrayIndex(objPoint.checkouts)];
-    features = getRandomInt(1, 6);
+    price = getRandomInteger(1000, 1000000);
+    type = objPoint.types[getRandomInteger(0, objPoint.types.length)];
+    rooms = getRandomInteger(1, 5);
+    guests = getRandomInteger(10, 100);
+    checkin = objPoint.checkins[getRandomInteger(0, objPoint.checkins.length)];
+    checkout = objPoint.checkouts[getRandomInteger(0, objPoint.checkouts.length)];
+    features = getRandomInteger(1, 6);
     description = '';
     photos = [];
     maps[i] = {
@@ -82,48 +74,48 @@ var generateArrayMapPoints = function (objPoint, arrayLength) {
   return maps;
 };
 
-var createLabel = function (objMap) {
-  var buttonElement = similarMapTemplateLabel.cloneNode(true);
+var createLabel = function (adObject) {
+  var buttonElement = mapPinTemplate.cloneNode(true);
   var img = buttonElement.querySelector('img');
-  img.src = objMap.author.avatar;
-  buttonElement.style.left = objMap.location.x - img.width / 2 + 'px';
-  buttonElement.style.top = objMap.location.y - img.height / 2 + 'px';
+  img.src = adObject.author.avatar;
+  buttonElement.style.left = adObject.location.x - img.width / 2 + 'px';
+  buttonElement.style.top = adObject.location.y - img.height / 2 + 'px';
   return buttonElement;
 };
 
-var createAds = function (objMap) {
-  var adsElement = similarMapTemplateAds.cloneNode(true);
+var createAdsCardMarkup = function (adObject) {
+  var adsElement = mapCardTemplate.cloneNode(true);
   var translate = {flat: 'Квартира', house: 'Дом', bungalo: 'Бунгало'};
   var i;
   var feature = '';
-  adsElement.querySelector('h3').textContent = objMap.offer.title;
-  adsElement.querySelector('p>small').textContent = objMap.offer.address;
-  adsElement.querySelector('.popup__price').innerHTML = objMap.offer.price + ' &#x20bd;/ночь';
-  adsElement.querySelector('h4').textContent = translate[objMap.offer.type];
-  adsElement.querySelector('h4+p').textContent = objMap.offer.rooms + ' для ' + objMap.offer.guests + ' гостей';
-  adsElement.querySelector('h4+p+p').textContent = 'Заезд после ' + objMap.offer.checkin + ', выезд до ' + objMap.offer.checkout;
-  adsElement.querySelector('.popup__features+p').textContent = objMap.offer.description;
-  adsElement.querySelector('.popup__avatar').src = objMap.author.avatar;
-  for (i = 0; i < objMap.offer.features; i++) {
+  adsElement.querySelector('h3').textContent = adObject.offer.title;
+  adsElement.querySelector('p > small').textContent = adObject.offer.address;
+  adsElement.querySelector('.popup__price').innerHTML = adObject.offer.price + ' &#x20bd;/ночь';
+  adsElement.querySelector('h4').textContent = translate[adObject.offer.type];
+  adsElement.querySelector('h4 + p').textContent = adObject.offer.rooms + ' для ' + adObject.offer.guests + ' гостей';
+  adsElement.querySelector('h4 + p + p').textContent = 'Заезд после ' + adObject.offer.checkin + ', выезд до ' + adObject.offer.checkout;
+  adsElement.querySelector('.popup__features + p').textContent = adObject.offer.description;
+  adsElement.querySelector('.popup__avatar').src = adObject.author.avatar;
+  for (i = 0; i < adObject.offer.features; i++) {
     feature += '<li class="feature feature--' + adFeatures.features[i] + '"></li>';
   }
   adsElement.querySelector('.popup__features').innerHTML = feature;
   return adsElement;
 };
 
-var addElementsToTheMap = function (objMap, createElements) {
-  var fragmentButton = document.createDocumentFragment();
+var createMarkupFragment = function (adObject, createElements) {
+  var documentFragment = document.createDocumentFragment();
   var i;
   var mapLength;
-  for (i = 0, mapLength = objMap.length; i < mapLength; i++) {
-    fragmentButton.appendChild(createElements(objMap[i]));
+  for (i = 0, mapLength = adObject.length; i < mapLength; i++) {
+    documentFragment.appendChild(createElements(adObject[i]));
   }
-  return fragmentButton;
+  return documentFragment;
 };
 
-ads = generateArrayMapPoints(adFeatures, lengthMap);
-showElement(map);
-similarListElementLabel.appendChild(addElementsToTheMap(ads, createLabel));
-similarListElementAds.appendChild(addElementsToTheMap(ads, createAds));
+ads = generateArrayMapPoints(adFeatures, adsQuantity);
+map.classList.remove('map--faded');
+mapFiltersContainer.appendChild(createMarkupFragment(ads, createAdsCardMarkup));
+mapPinContainer.appendChild(createMarkupFragment(ads, createLabel));
 
 
